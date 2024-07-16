52d719ca79b27e0fb32dd849d2f901387f21474b
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-07-16 13:43:57 +0200

    update sitemap.xml rendering

--- www/src/controllers/Page.php
+++ www/src/controllers/Page.php
@@ -8,6 +8,7 @@ class Page
     {
         $page = match($parts[0]) {
             'robots.txt' => "robots.txt",
+            'sitemap.xml' => "sitemap.xml",
             'login'       => TEMPLATE_ROOT ."login.php",
             'roadmap'     =>TEMPLATE_ROOT . "roadmap.php",
             default   => TEMPLATE_ROOT ."home.php"
