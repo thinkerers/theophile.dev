eb44b4d20ee490746376c6c8a6c556242ee36cba
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-07-16 18:13:17 +0200

    remove robots and sitemap redirect from Page.php has it is now handled by htaccess

--- www/src/controllers/Page.php
+++ www/src/controllers/Page.php
@@ -9,7 +9,6 @@ class Page
         $page = match($parts[0]) {
             'robots.txt' => "robots.txt",
             'sitemap.xml' => "sitemap.xml",
-            'contact'     => TEMPLATE_ROOT ."contact.php",
             'login'       => TEMPLATE_ROOT ."login.php",
             'roadmap'     =>TEMPLATE_ROOT . "roadmap.php",
             default   => TEMPLATE_ROOT ."home.php"
