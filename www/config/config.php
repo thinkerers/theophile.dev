447fc12353dc6f16094c5d4ab0f441a3700d12cb
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-07-17 14:00:57 +0200

    add variables for each main directories

--- www/config/config.php
+++ www/config/config.php
@@ -1,9 +1,16 @@
 <?php
 define('DEV', true);                        // In development or live? Development = true | Live = false
-define('DOMAIN', 'https://theophile.dev/');   // Domain (used to create links in emails)
-define('ROOT_FOLDER', 'public');            // Name of document root folder (e.g. public, content, htdocs)
+define('DOMAIN', getenv('SITE_DOMAIN'));
 
 $this_folder   = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']));
 $parent_folder = dirname($this_folder);
-define("DOC_ROOT", $parent_folder . DIRECTORY_SEPARATOR . ROOT_FOLDER . DIRECTORY_SEPARATOR);
-define("TEMPLATE_ROOT", APP_ROOT . DIRECTORY_SEPARATOR .'templates' . DIRECTORY_SEPARATOR);
+
+define("PUBLIC_ROOT", $parent_folder . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
+define("TEMPLATES_ROOT", APP_ROOT . DIRECTORY_SEPARATOR .'templates' . DIRECTORY_SEPARATOR);
+define("SRC_ROOT", APP_ROOT . DIRECTORY_SEPARATOR .'src' . DIRECTORY_SEPARATOR);
+define("CONTROLLERS_ROOT", SRC_ROOT . DIRECTORY_SEPARATOR .'controllers' . DIRECTORY_SEPARATOR);
+define("LIB_ROOT", SRC_ROOT . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR);
+define("MODEL_ROOT", SRC_ROOT . DIRECTORY_SEPARATOR .'model' . DIRECTORY_SEPARATOR);
+
+define("DB_NAME", 'database.sqlite');
+define("DB_PATH", MODEL_ROOT . DB_NAME);
